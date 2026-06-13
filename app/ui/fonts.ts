e75d8c9c619b89db17b6e9fa37ded79c01046ae2
davidarueda18-sketch <davidarueda18@gmail.com>
2026-06-13 12:54:25 -0500

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Nunito_Sans, Fredoka } from "next/font/google";
+
+export const nunitoSans = Nunito_Sans({
+  variable: "--font-nunito-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"],
+  display: "swap",
+});
+
+export const fredoka = Fredoka({
+  variable: "--font-fredoka",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
